1d30a2b6a72d926c8f95d2fe0df5f7a99aedbc36
masaqqa <ma.saqqa@gmail.com>
2025-09-03 17:24:36 +0300

    add news pages

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ if (document.querySelector(".swiper-domains")) {
     },
     breakpoints: {
       576: { slidesPerView: 1, spaceBetween: 10 },
-      768: { slidesPerView: 2, spaceBetween: 20 },
+      768: { slidesPerView: 3, spaceBetween: 20 },
       992: { slidesPerView: 4, spaceBetween: 30 },
     },
   });
